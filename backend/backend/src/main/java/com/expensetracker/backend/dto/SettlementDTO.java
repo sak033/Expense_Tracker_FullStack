444f67d2bebf5fb871657e5200033796d18f6a42
sak003 <sakshikuthe336@gmail.com>
2026-05-08 21:16:44 +0530

    fix 1

--- backend/backend/src/main/java/com/expensetracker/backend/dto/SettlementDTO.java
+++ backend/backend/src/main/java/com/expensetracker/backend/dto/SettlementDTO.java
@@ -6,7 +6,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SettlementDTO {
+
+    private Long id;
+
     private String from;
+
     private String to;
+
     private double amount;
 }
