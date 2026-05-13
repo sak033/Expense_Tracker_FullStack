a03a7e2e096d25638c096a544f360420df76d26d
sak003 <sakshikuthe336@gmail.com>
2026-05-13 13:12:37 +0530

    Fixed genini model

--- backend/backend/src/main/java/com/expensetracker/backend/service/GeminiService.java
+++ backend/backend/src/main/java/com/expensetracker/backend/service/GeminiService.java
@@ -19,7 +19,7 @@ public class GeminiService {
         try {
 
             String endpoint =
-                    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
+                    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
                             + apiKey;
 
             URL url = new URL(endpoint);
